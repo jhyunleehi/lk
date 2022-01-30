4b00093bae817c58f52b8b70311d83f7a262b104
jhyunlee <jhyunleehi@naver.com>
2022-01-30 14:10:39 +0900

    ok

--- code/malloc.c
+++
@@ -1,9 +0,0 @@
-#include<stdio.h>
-#include<malloc.h>
-int main (){
-    char *temp;
-    for (int i=0; i<1024*1024*10; i++){
-        temp=(char*)malloc(sizeof(char)*1024);
-        printf("%d K\n",i);
-    }
-}
